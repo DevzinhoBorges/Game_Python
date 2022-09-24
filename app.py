aed1557c4cd1eb052c564aca16a951854fed80d4
José Borges Costa <borgesgamedev@gmail.com>
2022-09-23 21:47:29 -0300

    First commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import pygame as pg
+
+pg.init()
+screen = pg.display.set_mode([840, 480])
+pg.display.set_caption("Spadown")
+
+def draw():
+  screen.fill((19,173,235))
+
+gameloop = True
+if __name__ == "__main__":
+  while gameloop:
+    for event in pg.event.get():
+      if event.type == pg.QUIT:
+        gameloop = False
+
+    draw()
+    pg.display.update()
