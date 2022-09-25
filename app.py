b46916a66855b3fe837c2c951b894e69b45355ef
José Borges Costa <borgesgamedev@gmail.com>
2022-09-25 17:52:23 -0300

    Modificações no codigo

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import pygame as pg
 
-pg.init()
-screen = pg.display.set_mode([840, 480])
-pg.display.set_caption("Spacedown")
+pg.init() # Inicializa o pygame
+screen = pg.display.set_mode([840, 480]) # Cria a tela para exibição do jogo
+pg.display.set_caption("Spacedown") # Defini o nome na barra de titulo da pagina
 
-def draw():
-  screen.fill((19,173,235))
+drawGroup = pg.sprite.Group() # Cria um grupo para adição de sprites
+
+player = pg.sprite.Sprite(drawGroup) # Insere o sprite ao grupo
+player.image = pg.image.load("img/nave.png") # Adiciona a imagem do sprite
+player.image = pg.transform.scale(player.image, [100, 100]) # Modifica o tamanho da imagem
+player.rect = pg.Rect(50, 50, 100, 100) # Defini o tamanho e a posição da imagem
+
+def draw(): # Função para desenhar objetos na tela
+  screen.fill((19,173,235)) # Defini a cor de fundo da tela
+  drawGroup.draw(screen) # Desenha os sprites na tela
 
 gameloop = True
 if __name__ == "__main__":
